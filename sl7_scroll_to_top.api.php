535c85bfada449897b6d8b026aa6254d217e5899
Semyon Dragunov <sam.dragunov@gmail.com>
2018-04-13 04:07:24 +0500

    UPD api doc

--- sl7_scroll_to_top.api.php
+++ sl7_scroll_to_top.api.php
@@ -10,9 +10,9 @@
  * settings - Не обязательно. Форма с настройками. По Form API, но единственный аргумент $default_value,
  * в нём передаются параметры по умолчанию.
  * @see sl7_scroll_to_top_theme_default_settings().
- * img - Ихображение для превью.
- * js - JavaScript файл.
- * css - CSS файл.
+ * img - Изображение для превью.
+ * js - Не обязательно. JavaScript файл.
+ * css - Не обязательно. CSS файл.
  */
 function hook_sl7_scroll_to_top_theme() {
 	$path = drupal_get_path('module', 'sl7_scroll_to_top');
